7135ca181f6df2838dc5e17d0f2ee645e9b6d068
Nandinist <nandini160913@gmail.com>
2022-10-18 16:15:08 +0530

    style : bg color

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var btnTranslate = document.querySelector("#btn-translate");
+var btnTrans = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+var output = document.querySelector("#output");
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
@@ -10,7 +10,7 @@ function getTranslationURL(input) {
 
 function errorHandler(error) {
   console.log("error occured", error);
-  alert("something wrong with server! try again after some time");
+  alert("Something wrong with server! Try again after some time.");
 }
 
 function clickHandler() {
@@ -20,9 +20,9 @@ function clickHandler() {
     .then((response) => response.json())
     .then((json) => {
       var translatedText = json.contents.translated;
-      outputDiv.innerText = translatedText;
+      output.innerText = translatedText;
     })
     .catch(errorHandler);
 }
 
-btnTranslate.addEventListener("click", clickHandler);
+btnTrans.addEventListener("click", clickHandler);
